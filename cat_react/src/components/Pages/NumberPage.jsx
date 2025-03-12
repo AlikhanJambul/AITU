7a047322b9b1810e9aa55d2b676ad2868c2a3857
aljambul <jambul9827@gmail.com>
2025-03-12 18:28:12 +0500

    add: card with cat info

--- cat_react/src/components/Pages/NumberPage.jsx
+++ cat_react/src/components/Pages/NumberPage.jsx
@@ -1,39 +1,94 @@
 import catOne from '../../image/cat-one.png'
-import paw from '../../image/icons/paw_white.png'
+import catTwo from '../../image/cat-two.png'
+import catThree from '../../image/cat-three.png'
+import catFour from '../../image/cat-four.png'
+import catFive from '../../image/cat-five.png'
+import catSix from '../../image/cat-six.png'
+import CardNumber from '../Pages/Page_components/Card_number'
+import catBg from '../../image/cat-banner.png'
 
 function NumberPage() {
     const catInfo = [
         {
+            title: "Эконом",
             catPhoto: catOne,
             size: "90х70х180 см",
             plosh: "0,63 м2",
+            osnNum: "1",
             price: "100 P"
-        }
+        },
+        {
+            title: "Эконом плюс",
+            catPhoto: catTwo,
+            size: "90х100х180 см",
+            plosh: "0,90 м2",
+            osnNum: "2, 3",
+            price: "200 P"
+        },
+        {
+            title: "Комфорт",
+            catPhoto: catThree,
+            size: "100х125х180 см",
+            plosh: "1,13 м2",
+            osnNum: "2, 3, 4",
+            price: "250 P"
+        },
+        {
+            title: "Супер-Люкс",
+            catPhoto: catFour,
+            size: "180х160х180 см",
+            plosh: "2,88 м2",
+            osnNum: "2, 3, 4, 5",
+            price: "600 P"
+        },
+        {
+            title: "Сьют",
+            catPhoto: catFive,
+            size: "125х125х180 см",
+            plosh: "1,56 м2",
+            osnNum: "2, 3, 4",
+            price: "350 P"
+        },
+        {
+            title: "Люкс",
+            catPhoto: catSix,
+            size: "160х160х180 см",
+            plosh: "2,56 м2",
+            osnNum: "2, 3, 4, 5",
+            price: "500 P"
+        },
+        {
+            title: "Эконом",
+            catPhoto: catOne,
+            size: "90х70х180 см",
+            plosh: "0,63 м2",
+            osnNum: "1",
+            price: "100 P"
+        },
+        {
+            title: "Эконом плюс",
+            catPhoto: catTwo,
+            size: "90х100х180 см",
+            plosh: "0,90 м2",
+            osnNum: "2, 3",
+            price: "200 P"
+        },
     ]
 
 
     return (
         <section className='card2'>
-            <div className='card-cat'>
-                <img className='card-cat__img' src={catOne} alt="cat-one" />
-                <p className='card-cat__title'><b>Эконом</b></p>
-                <ul className='card-cat__list'>
+            <p className='card2__title'>Наши номера</p>
+            <ul className='card-cat'>
+                {catInfo.map((info) => (
                     <li>
-                        <p className='card-cat__size'>Размеры (ШхГхВ) - 90х70х180 см</p>
+                        <CardNumber title={info.title} catPhoto={info.catPhoto} size={info.size} plosh={info.plosh} osnNum={info.osnNum} price={info.price} />
                     </li>
-                    <li>
-                        <p className='card-cat__plos'>Площадь - 0,63 м2</p>
-                    </li>
-                    <li>
-                        <p className='card-cat__price'>Цена за сутки: <b>100 Р</b></p>
-                    </li>
-                </ul>
-                <div className='button-alternative__replace'>
-                    <button className="button button-alternative button-alternative__none-margin">
-                        <span className="button__text">Забронировать</span>
-                        <img className="button__item" src={paw} alt="paw" width="35px" height="35px" />
-                    </button>
-                </div>
+                ))}
+            </ul>
+
+            <div className='container-cat'>
+                <img src={catBg} alt="bg cat" />
             </div>
         </section>
     );
--- cat_react/src/components/Pages/Page_components/Card_number.jsx
+++ cat_react/src/components/Pages/Page_components/Card_number.jsx
@@ -1,16 +1,19 @@
 import paw from '../../../image/icons/paw_white.png'
 
-function CardNumber({ catPhoto, size, plosh, price }) {
+function CardNumber({ title, catPhoto, size, plosh, osnNum, price }) {
     return (
-        <>
+        <article className='card-cat__article'>
             <img className='card-cat__img' src={catPhoto} alt="cat-one" />
-            <p className='card-cat__title'><b>Эконом</b></p>
+            <p className='card-cat__title'><b>{title}</b></p>
             <ul className='card-cat__list'>
                 <li>
                     <p className='card-cat__size'>Размеры (ШхГхВ) - {size}</p>
                 </li>
                 <li>
                     <p className='card-cat__plos'>Площадь - {plosh}</p>
+                </li>
+                <li>
+                    <p className='card-cat_osnNum'>Оснащение номера: {osnNum}</p>
                 </li>
                 <li>
                     <p className='card-cat__price'>Цена за сутки: <b>{price}</b></p>
@@ -22,6 +25,8 @@ function CardNumber({ catPhoto, size, plosh, price }) {
                     <img className="button__item" src={paw} alt="paw" width="35px" height="35px" />
                 </button>
             </div>
-        </>
+        </article>
     )
 }
+
+export default CardNumber;
